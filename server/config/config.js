6824442b9aa10e2e9aafaed5552f3def3a2749b8
Raul_Cordeiro <79949012+Raul-cordeiro@users.noreply.github.com>
2024-03-25 19:16:43 -0300

    okk

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+// config/config.js
+
+module.exports = {
+    db: {
+      host: 'localhost', 
+      port: 5000, 
+      dbName: 'incasadb', 
+      username: 'root', 
+      password: '' 
+      
+    }
+  };
+  
